a71731fdf857fafb8f48db82ed6dbdaadd08c3d8
Tom Shafer <tj@tjshafer.com>
2024-10-04 11:48:42 -0700

    remove api

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -86,10 +86,10 @@
                             <svg class="w-6 h-6 mr-2 transition-transform duration-300 group-hover:animate-bounce" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8h1a4 4 0 0 1 0 8h-1"/><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"/><line x1="6" y1="1" x2="6" y2="4"/><line x1="10" y1="1" x2="10" y2="4"/><line x1="14" y1="1" x2="14" y2="4"/></svg>
                             <span>Recipes</span>
                         </a></li>
-                        <li><a href="/api" class="flex items-center transition-all duration-300 hover:text-gray-100 hover:transform hover:-translate-y-0.5">
+                        <!-- <li><a href="/api" class="flex items-center transition-all duration-300 hover:text-gray-100 hover:transform hover:-translate-y-0.5">
                             <svg class="w-6 h-6 mr-2 transition-transform duration-300 group-hover:animate-bounce" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/></svg>
                             <span>API Docs</span>
-                        </a></li>
+                        </a></li> -->
                     </ul>
                 </nav>
             </div>
