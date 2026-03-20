07abf4b244628711afbbe46a201d214a3af445e2
Tom Shafer <tj@tjshafer.com>
2026-03-20 10:56:11 -0700

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import path from 'path';
+
+const host = 'placeholder.test';
+
+function detectTlsCerts() {
+    // Laravel Herd
+    const herdPath = path.resolve(process.env.HOME, `Library/Application Support/Herd/config/valet/Certificates/${host}.crt`);
+    const herdKeyPath = path.resolve(process.env.HOME, `Library/Application Support/Herd/config/valet/Certificates/${host}.key`);
+    if (fs.existsSync(herdPath)) {
+        return { cert: herdPath, key: herdKeyPath };
+    }
+
+    // Laravel Valet
+    const valetPath = path.resolve(process.env.HOME, `.config/valet/Certificates/${host}.crt`);
+    const valetKeyPath = path.resolve(process.env.HOME, `.config/valet/Certificates/${host}.key`);
+    if (fs.existsSync(valetPath)) {
+        return { cert: valetPath, key: valetKeyPath };
+    }
+
+    return false;
+}
 
 export default defineConfig({
     plugins: [
@@ -9,6 +31,10 @@ export default defineConfig({
         }),
     ],
     server: {
-        https: process.env.APP_ENV === 'production' || process.env.VITE_HTTPS === 'true',
+        host,
+        https: detectTlsCerts(),
+        hmr: {
+            host,
+        },
     },
 });
